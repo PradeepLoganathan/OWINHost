875a5cf83cc8f87faf07eda8f97c28c5deb8f180
Pradeep Loganathan <pradeep.loganathan@hotmail.com>
2018-05-13 20:47:55 +0530

    Add project files.

---
+++ OWINHost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+
+namespace OWINHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                 .UseKestrel()
+                 .UseContentRoot(Directory.GetCurrentDirectory())
+                 .UseStartup<Startup>()
+                 .Build();
+
+            host.Run();
+                
+        }
+    }
+}
---
+++ OWINHost/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Builder;
+
+
+namespace OWINHost
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Middleware One</br>");
+                await next.Invoke();
+            });
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Middleware Two</br>");
+                await next.Invoke();
+            });
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Middleware Three</br>");
+                await next.Invoke();
+            });
+        }
+    }
+}
