ac8021ad53bc1f732baf1fc53d3aee9be2fba29c
Pradeep Loganathan <pradeep.loganathan@hotmail.com>
2018-05-13 22:13:09 +0530

    Mapwhen changes

--- OWINHost/StartUp.cs
+++ OWINHost/StartUp.cs
@@ -25,6 +25,30 @@ namespace OWINHost
                 await context.Response.WriteAsync("Middleware Three</br>");
                 await next.Invoke();
             });
+
+            app.MapWhen( context => context.Request.Query.ContainsKey("querypath1"), (appbuilder) =>
+            {
+                appbuilder.Use(async (context, next) =>
+                {
+                    await context.Response.WriteAsync("-- Map when -- querypath1 - Middleware One</br>");
+                });
+            });
+
+
+            app.Map("/branch1", (appBuilder) =>
+             {
+                 appBuilder.Use(async (context, next) =>
+                 {
+                     await context.Response.WriteAsync("--Branch 1 - Middleware One</br>");
+                     await next.Invoke();
+                 });
+
+                 appBuilder.Use(async (context, next) =>
+                 {
+                     await context.Response.WriteAsync("--Branch 2 - Middleware Two</br>");
+                     await next.Invoke();
+                 });
+             });
         }
     }
 }
